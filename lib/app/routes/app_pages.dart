d03c4e4cd1577116de4d189831c457681fcfb888
rilwanat <49300698+rilwanat@users.noreply.github.com>
2025-01-17 18:49:02 +0100

    created app icon and launcher icon

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -3,6 +3,8 @@ import 'package:get/get.dart';
 import '../modules/auth/bindings/auth_binding.dart';
 import '../modules/auth/views/auth_view.dart';
 import '../modules/home/bindings/home_binding.dart';
+import '../modules/home/bindings/home_binding.dart';
+import '../modules/home/views/home_view.dart';
 import '../modules/home/views/home_view.dart';
 import '../modules/onboarding/bindings/onboarding_binding.dart';
 import '../modules/onboarding/views/onboarding_view.dart';
@@ -19,6 +21,13 @@ class AppPages {
       name: _Paths.HOME,
       page: () => const HomeView(),
       binding: HomeBinding(),
+      children: [
+        GetPage(
+          name: _Paths.HOME,
+          page: () => const HomeView(),
+          binding: HomeBinding(),
+        ),
+      ],
     ),
     GetPage(
       name: _Paths.ONBOARDING,
--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -6,6 +6,7 @@ abstract class Routes {
   static const HOME = _Paths.HOME;
   static const ONBOARDING = _Paths.ONBOARDING;
   static const AUTH = _Paths.AUTH;
+  static const HOME = _Paths.HOME;
 }
 
 abstract class _Paths {
@@ -13,4 +14,5 @@ abstract class _Paths {
   static const HOME = '/home';
   static const ONBOARDING = '/onboarding';
   static const AUTH = '/auth';
+  static const HOME = '/home';
 }
